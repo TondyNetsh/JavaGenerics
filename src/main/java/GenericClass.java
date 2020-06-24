3501b1b4c295d359aa75bda944c2c25a03e9a53b
Rotondwa Netshimbupfe <59872053+TondyNetsh@users.noreply.github.com>
2020-06-24 14:34:13 +0200

    Java Generics

--- src/main/java/GenericClass.java
+++ src/main/java/GenericClass.java
@@ -19,23 +19,6 @@ class GenericClass<T>{
             System.out.print(elements + " "); //Output for all string and/or integer elements
         }
         System.out.println("");
-    }
-
-    public static void main(String[] args) {
-
-        GenericClass generic = new GenericClass();
-
-        generic.printArrayContent(new Integer[]{1,2,3});
-        generic.printArrayContent(new String[]{"Rotondwa", "Netshimbupfe", "Gauteng"});
-
-        GenericClass<String> stringInstance = new GenericClass<String>();
-        stringInstance.setStringElements("Test"); //Assign a value to string element from Generic class
-
-        System.out.println(stringInstance.getStringElements()); // print out Test
-
-        GenericClass<Integer> integerInstance = new GenericClass<Integer>();
-        integerInstance.setIntElements(1000); //Assign a value to intiger element from Generic class
-
-        System.out.println(integerInstance.getIntElements()); // print out 1000
+        //System.out.print();
     }
 }
